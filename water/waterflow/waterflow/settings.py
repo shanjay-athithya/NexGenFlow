8dede5b4f88981e04f4049ce9e8e0b8fcf122f65
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-09-01 22:11:58 +0530

    v8

--- water/waterflow/waterflow/settings.py
+++ water/waterflow/waterflow/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -145,7 +145,7 @@ EMAIL_HOST_PASSWORD = ''  # Your email password
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'water/static'), 
+    os.path.join(BASE_DIR, 'static'), 
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
